766f64cb2f959d6a55244f8d2978858f7ac18a59
Ayush Pandey <payush351@gmail.com>
2024-04-04 21:28:08 +0530

    Update ViewUser.js

--- Frontend/src/users/ViewUser.js
+++ Frontend/src/users/ViewUser.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function ViewUser() {
@@ -21,31 +21,30 @@ export default function ViewUser() {
   };
 
   return (
-    <div className="container">
+    <div className="container" style={{ marginTop: "50px", padding: "20px", background: 'rgba(255, 255, 255, 0.15)', backdropFilter: 'blur(10px)', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)' }}>
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">User Details</h2>
 
           <div className="card">
             <div className="card-header">
-              Details of user id : {user.id}
               <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <b>Name:</b>
-                  {user.name}
+              <li className="list-group-item">
+                  <b>User Id:</b> {user.id}
                 </li>
                 <li className="list-group-item">
-                  <b>UserName:</b>
-                  {user.username}
+                  <b>Name:</b> {user.name}
                 </li>
                 <li className="list-group-item">
-                  <b>Email:</b>
-                  {user.email}
+                  <b>UserName:</b> {user.username}
+                </li>
+                <li className="list-group-item">
+                  <b>Email:</b> {user.email}
                 </li>
               </ul>
             </div>
           </div>
-          <Link className="btn btn-primary my-2" to={"/"}>
+          <Link style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}className="btn btn-outline-primary my-2" to={"/"}>
             Back to Home
           </Link>
         </div>
